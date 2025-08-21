f9b7411fb3d456758dcfd8d5087f5023713b20b2
NeuralKaizen <neuralkaizen@gmail.com>
2025-08-21 11:49:57 -0500

    Cambios Est-1

--- App/view.py
+++ App/view.py
@@ -55,6 +55,7 @@ def print_menu():
     print("Opciones:")
     print("1- Cargar Libros")
     print("2- Cargar Tags")
+    print("3- Cargar Tags de Libros")
     # TODO: Mods de Est-1, Est-2 y Est-3 en el Lab 2
     # Agregue la opción 3 para cargar los tags de los libros.
     # Pueede guiarse de las opciones 1 y 2.
@@ -95,16 +96,20 @@ def load_books_tags(app):
     :param app: Aplicación de la lógica
     :type app: logic
     """
+    book_tags = logic.load_books_tags(app,
+                             "GoodReads/book_tags-small.csv")
+
+    return book_tags
     # TODO: Mods de Est-1, Est-2 y Est-3 en el Lab 2
-    pass
 
 
 def first_book(app):
     """
     Devuelve el primer libro cargado en el conjunto de libros
     """
+    first = logic.first_book(app)
     # TODO: Mods de Est-1, Est-2 y Est-3 en el Lab 2
-    pass
+    return first
 
 
 def last_book(app):
@@ -138,7 +143,8 @@ def main():
             print("Total de libros cargados: " + str(books) + "\n")
 
             # TODO: Mods de Est-1 en el Lab 2
-            first = None
+            first = first_book(app)
+            print ("Primer libro cargado:\n" + str(first) + "\n")
 
             # TODO: Mods de Est-2 en el Lab 2
             last = None
